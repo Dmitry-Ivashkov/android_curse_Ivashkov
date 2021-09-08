1d5bb48f51c2ac200f4046fed9afb21b5948f34b
Ивашков Дмитрий Александрович <100100101104@mail.ru>
2021-09-08 16:12:12 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android_curse/UserAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.android_curse
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class UserAdapter : RecyclerView.Adapter<UserAdapter.ViewHolder>() {
+    
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class ViewHolder :RecyclerView.ViewHolder(){
+
+    }
+}
